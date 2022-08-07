088e4f180ea419b1bce3dee85874c6d0745a3718
Kristina Salnyk <kristina.salnyk@gmail.com>
2022-08-08 00:33:05 +0200

    Added script for first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtnRef = document.querySelector('[data-start]');
+const stopBtnRef = document.querySelector('[data-stop]');
 
+let changeColorIntervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startBtnClickHandler = () => {
+  changeColorIntervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  toggleBtnAccessibility();
+};
+
+const stopBtnClickHandler = () => {
+  clearInterval(changeColorIntervalId);
+  toggleBtnAccessibility();
+};
+
+const toggleBtnAccessibility = () => {
+  startBtnRef.toggleAttribute('disabled');
+  stopBtnRef.toggleAttribute('disabled');
+};
+
+stopBtnRef.disabled = true;
+
+startBtnRef.addEventListener('click', startBtnClickHandler);
+
+stopBtnRef.addEventListener('click', stopBtnClickHandler);
