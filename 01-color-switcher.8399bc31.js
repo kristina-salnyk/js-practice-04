bd060245d2fda7e368a2905868da0ac1f2a3cf17
kristina-salnyk <kristina.salnyk@gmail.com>
2022-08-13 08:31:33 +0000

    Deploying to gh-pages from @ kristina-salnyk/goit-js-hw-09@531b0f6859479c2a584d4a9846485e838a0f1796 🚀

--- 01-color-switcher.8399bc31.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")},n=null;var e=function(){t.startBtn.toggleAttribute("disabled"),t.stopBtn.toggleAttribute("disabled")};t.stopBtn.disabled=!0,t.startBtn.addEventListener("click",(function(){n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e()})),t.stopBtn.addEventListener("click",(function(){clearInterval(n),e()}))}();
-//# sourceMappingURL=01-color-switcher.8399bc31.js.map
